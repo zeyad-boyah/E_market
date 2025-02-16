220d8f2b89c89051fa1e23f38006930d078413eb
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-02-16 23:24:43 +0200

    add ItemsPage view

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from main.views import HomePage
+from main.views import HomePage, ItemsPage
 
 
 urlpatterns = [
-    path('home/', HomePage , name="home")
+    path('home/', HomePage , name="home"),
+    path('items/', ItemsPage , name="all_items")
 ]
--- main/views.py
+++ main/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def HomePage(request):
-    return render(request, template_name="main/main.html")
+    return render(request, template_name="main/home.html")
+
+def ItemsPage (request):
+    items= [
+        {
+            'name': 'Phone',
+            'price': '600'
+        },
+        {
+            'name': 'Laptop',
+            'price': '1000'
+        }
+    ]
+
+    
+    return render(request, template_name="main/items.html", context={'items':items})
