08900e4b9f7c607ed6086d3ea0c1bde1ddbffc2a
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-07-02 04:09:33 +0300

    depricates the ownership column and adds a stock one

--- main/models.py
+++ main/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
+    stock = models.IntegerField(default=100)
     description = models.CharField(max_length=2000)
     image_url = models.CharField(max_length=256)
-    owner = models.ForeignKey(User, default=None, blank=True, on_delete=models.CASCADE, null=True)
-    
+
     def __str__(self):
         return self.name
